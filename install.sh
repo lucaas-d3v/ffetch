7a57046c30bfe9ae1fcd428b61f7d3edfa4b5fdd
lucaas-d3v <lucas.workps@gmail.com>
2026-04-07 14:58:19 -0300

    0.0.2

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ echo "Compiling ffetch"
 flint build ffetch.fl
 
 echo "Installing ffetch"
+
+sudo mkdir -p /usr/share/ffetch/assets
+
+sudo cp assets/* /usr/share/ffetch/assets
 sudo mv ffetch /usr/local/bin
 
 echo "Ok"
